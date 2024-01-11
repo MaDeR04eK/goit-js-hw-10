3385f181ee705837523dd39fa828c40a170982de
Kyryl <MaDeR04eK@users.noreply.github.com>
2024-01-11 20:15:18 +0100

    add function convertMs

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,8 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-let userSelectedDate
+let userSelectedDate;
+let countdownInterval;
 
 const options = {
   enableTime: true,
@@ -13,11 +14,9 @@ const options = {
       userSelectedDate = selectedDates[0];
 const currentDate = new Date();
     if (userSelectedDate < currentDate) {
-      // Вивести повідомлення та зробити кнопку неактивною
       window.alert("Please choose a date in the future");
       mainBtn.disabled = true;
     } else {
-      // Зробити кнопку активною
       mainBtn.disabled = false;
     }
   },
@@ -33,3 +32,28 @@ mainBtn.addEventListener('click', () => {
     window.alert("Please choose a valid date in the future");
   }
 });
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
