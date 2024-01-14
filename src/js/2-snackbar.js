c3b695b6667d29e7ff3644fff38a1cd71bdf12b7
Kyryl <MaDeR04eK@users.noreply.github.com>
2024-01-14 08:59:17 +0100

    finished task-2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { state, delay } = form.elements;
+  const selectedState = Array.from(state).find(radio => radio.checked);
+  console.log(selectedState.value);
+  getPromise(delay.value, selectedState.value)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+}
+
+function getPromise(delay, selectedState) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
